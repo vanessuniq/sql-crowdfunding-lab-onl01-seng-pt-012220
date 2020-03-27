dff111344816e0da64fd304b061a842ed192e119
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-27 05:29:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,6 +20,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   FROM pledges
   LEFT JOIN users
   ON users.id = pledges.user_id
+  GROUP by users.name
   ORDER BY users.name;"
 end
 
