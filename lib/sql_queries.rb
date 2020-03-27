d15bd1e6eb6b3b01fa851042c5b46f76ec80b0f5
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-27 05:20:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, pledges.amount
   FROM projects
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON projects.id = pledges.project_id
   ORDER BY projects.title;"
 end
