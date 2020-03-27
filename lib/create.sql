633d024476cc30bc0c35f09e0f5b53ef26c766e6
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-27 05:19:26 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT;
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
 CREATE TABLE users (id INTEGER primary KEY, name TEXT, age INTEGER);
 
