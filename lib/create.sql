145ede0a4bbcc1071e56859b06e3495bd5281160
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-27 05:15:08 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
-create table projects (id INT PRIMARY KEY, title TEXT, category TEXT, funding_goal INT, start_date varchar(10), end_date varchar(10));
+CREATE TABLE projects (id INT PRIMARY KEY, title TEXT, category TEXT, funding_goal INT, start_date varchar(10), end_date varchar(10));
 
-create table users (id INT primary KEY, name varchar(25), age INT);
+CREATE TABLE users (id INT primary KEY, name varchar(25), age INT);
 
 CREATE TABLE pledges (id INT PRIMARY KEY, amount INT, user_id INT, project_id INT);
