764cf68645d0f1903170626c1740fbc46a8dc694
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-27 05:26:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   FROM projects
   LEFT JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY SUM(pledges.amount)
+  GROUP BY projects.title
   ORDER BY projects.title;"
 end
 
